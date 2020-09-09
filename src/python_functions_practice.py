6271812551bb7a28364299a71c62fdca4f684f07
AmyBrowneDesigns <amybrowne20@gmail.com>
2020-09-09 17:05:18 +0100

    run tests functions task

---
+++ src/python_functions_practice.py
@@ -0,0 +1,40 @@
+def return_10():
+    return 10
+
+
+def add(num_1, num_2):
+    sum = num_1 + num_2
+    return sum
+
+
+def subtract(num_1, num_2):
+    sum = num_1 - num_2
+    return sum
+
+
+def multiply(num_1, num_2):
+    sum = num_1 * num_2
+    return sum
+
+
+def divide(num_1, num_2):
+    sum = num_1 / num_2
+    return sum
+
+
+def length_of_string(test_string):
+    return len(test_string)
+
+def join_string(string_1, string_2):
+    return string_1 + string_2
+    
+
+def add_string_as_number(num1, num2):
+    # num1 = "1"
+    # num2 = "2"
+    int(num1) 
+    int(num2)
+    sum = num1 + num2
+    return sum
+
+
